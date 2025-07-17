e83ad4574497cf823acc0a63072132cc5a6333b5
Juna <itujun7@gmail.com>
2025-07-17 21:02:50 +0700

    upgrade major dependency

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/itujun/golang-app-say-hello
 
 go 1.24.4
 
-require github.com/itujun/golang-modules-say-hello v1.0.1
+require github.com/itujun/golang-modules-say-hello/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	golang_modules_say_hello "github.com/itujun/golang-modules-say-hello"
+	golang_modules_say_hello "github.com/itujun/golang-modules-say-hello/v2"
 )
 
 func main() {
-	fmt.Println(golang_modules_say_hello.SayHello())
+	fmt.Println(golang_modules_say_hello.SayHelloi("jun"))
 }
